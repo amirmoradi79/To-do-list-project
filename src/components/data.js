ef119dd0dce89646863d5208f2b98304d5016e22
amir moradi <amir.moradi.work1379@gmail.com>
2024-07-18 16:19:09 +0330

    amir commit

---
+++ src/components/data.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    article: "take out the trash",
+    status: "done",
+    id: 1,
+  },
+  {
+    article: "take out the trash",
+    status: "undone",
+    id: 2,
+  },
+  {
+    article: "take out the trash",
+    status: "done",
+    id: 3,
+  },
+];
--- src/components/main/panel/panel.jsx
+++ src/components/main/panel/panel.jsx
@@ -1,29 +1,42 @@
-import './panel.css'
+import { data } from "../../data";
+import "./panel.css";
 
 export function Panel() {
+  let filteredTasks = data.filter((task) => task.status.includes(""));
+  return (
+    <div className="container">
+      <div className="panle">
+        <div className="input-border">
+          <input
+            className="input-nameList"
+            type="text"
+            placeholder="Add to Here"
+          />
+          <button className="button-add">+</button>
+        </div>
+        <div>
+          <select name="categories" id="categories" className="categories">
+            <option value="all">All</option>
+            <option value="done">Done</option>
+            <option value="undone">Undone</option>
+          </select>
+        </div>
+      </div>
+      <div className="list"></div>
+    </div>
+  );
+}
+{
+  /* <div className="task">
+          <p>take out the trash</p>
+          <div className="btns">
+            <button>
+              <img src="../../public/Actions-dialog-ok-apply-icon.png" />
+            </button>
 
-    return (
-        <>
-
-            <div className='panle'>
-                <div className="input-border">
-                    <input className="input-nameList" type="text" placeholder="Add to Here" />
-                    <button className="button-add">+</button>
-                </div>
-                <div>
-                    <select name="categories" id="categories" className='categories'>
-                        <option value="all">All</option>
-                        <option value="done">Done</option>
-                        <option value="undone">Undone</option>
-                    </select>
-                </div>
-            </div> 
-            <div className='tasks'> 
-                
-
-            </div>
-
-        </>
-    )
-
+            <button>
+              <img src="../../public/trash-icon.png" />
+            </button>
+          </div>
+        </div> */
 }
